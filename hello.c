a8b0f7ce7e8da8f0bc43b2587c4f0a92250dcefa
pranav <pnikam2@huskers.unl.edu>
2020-08-24 17:20:43 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Pranav Nikam
+ *    pnikam2@unl.edu
+ * Date: 2020/07/17
+ *
+ * A simple name major in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+     printf("my name is Pranav Nikam.\n");
+     printf("computer science.\n");
+     
+     return  0;
+}
